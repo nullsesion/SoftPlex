0c4a59b68c7fda6b53ebf38c5396e2db6f3dc986
Ivanov E. V. <nullsesion@yandex.ru>
2024-10-28 17:57:04 +0300

    fix

--- SoftPlex/App-Docker-InstallOrReinstall-And-Start.bat
+++ SoftPlex/App-Docker-InstallOrReinstall-And-Start.bat
@@ -7,4 +7,4 @@ docker-compose -f docker/docker-compose.yml up -d
 TIMEOUT /T 5
 start "" http://localhost:5080/swagger/index.html
 TIMEOUT /T 5
-start "" http://localhost:5081/swagger/index.html
+start "" http://localhost:5081/
